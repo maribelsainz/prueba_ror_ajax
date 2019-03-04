3170b3e010943622f7d09fa8d05ffe2b1fd6f021
“Maribel” <maribel.sainzc@gmail.com>
2019-03-04 12:04:02 -0300

    con_destroy_y_edit

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,38 @@
+class CompaniesController < ApplicationController
+  def index
+    @companies = Company.order(:id)
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(
+      name: params[:company][:name],
+      # user_id: current_user.id
+    )
+    @company.save
+    respond_to :js
+  end
+
+  def destroy
+    @company = Company.find(params[:id])
+    @company.destroy
+    respond_to :js
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+    respond_to :js
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    @company.name = params[:company][:name]
+    @company.save
+    respond_to :js
+  end
+
+  def show
+    @company = Company.find(params[:id])
+    respond_to :js
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :companies
+  devise_for :users
+ root 'companies#index'
+end
